8b181e0fc38d2ec726fc85e380910fef1c0c9542
gabrihassan04 <118894687+gabrihassan04@users.noreply.github.com>
2023-01-18 10:14:28 +0100

    server

---
+++ gale/Server/forza4.java
@@ -0,0 +1,24 @@
+public class forza4 {
+    
+boolean iniziato = false;
+boolean turno = false ; //FALSE = GIOCATORE 1 - TRUE = GIOCATORE 2
+int [][] = new int [6][7];
+
+
+boolean vittoria=false;
+
+public void play(){
+    if(iniziato==true)
+    {
+
+    }
+
+
+}
+
+
+
+
+
+
+}
